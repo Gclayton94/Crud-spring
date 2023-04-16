58dd67e432e3242556812e1a870b37ee7c471768
Gclayton94 <giovanni.gcda@gmail.com>
2023-04-16 13:19:55 -0300

    feat: criando DTO

---
+++ src/main/java/com/desafiounigex/products/dto/ProductDTO.java
@@ -0,0 +1,13 @@
+package com.desafiounigex.products.dto;
+
+
+public class ProductDTO {
+    private Long id;
+
+    private  String nome;
+
+
+    private  String codigo;
+
+    private  Integer quantidade;
+}
--- src/main/java/com/desafiounigex/products/service/ProductServiceImplement.java
+++ src/main/java/com/desafiounigex/products/service/ProductServiceImplement.java
@@ -1,7 +1,9 @@
 package com.desafiounigex.products.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class ProductServiceImplement implements ProductService {
 }
